36fcad574e431d5556836cf027d5c2f6d7b4044e
Varun Sharma <imva508@gmail.com>
2019-02-16 21:18:20 +0530

    bound to product with layout

--- app/src/main/java/codingwithmitch/com/databindinggettingstarted/MainActivity.java
+++ app/src/main/java/codingwithmitch/com/databindinggettingstarted/MainActivity.java
@@ -5,15 +5,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import codingwithmitch.com.databindinggettingstarted.databinding.ActivityMainBinding;
+import codingwithmitch.com.databindinggettingstarted.models.Product;
+import codingwithmitch.com.databindinggettingstarted.util.Products;
 
 public class MainActivity extends AppCompatActivity {
 
     // Data binding
     private ActivityMainBinding mBinding;
+    private Product mProduct;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        Products products = new Products();
+        mProduct = products.PRODUCTS[1];
+        mBinding.setProduct(mProduct);
     }
 }
